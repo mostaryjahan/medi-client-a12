3838ffe833384c79724cebad321c55eb6e9cfbf6
Mostary Jahan <mostaryjahan01@gmail.com>
2024-06-02 09:54:09 +0600

    added shop page

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
 import useCategory from "../../Hook/useCategory";
 import { IoEye } from "react-icons/io5";
-
+import { useEffect, useRef, useState } from "react";
 
 const Shop = () => {
-    const [category] = useCategory();
+  const [category] = useCategory();
 
-    return (
-        <div>
-           <h1 className="text-center text-2xl md:text-3xl lg:text-5xl font-bold">Shop</h1>
+  const [selectedItem, setSelectedItem] = useState(null);
+  //  console.log(selectedItem);
+  const modalRef = useRef(null);
 
-            <div className="overflow-x-auto ">
+  
+  const openModal = (shop) => {
+    setSelectedItem(shop);
+    document.getElementById('my_modal_5').showModal();
+  };
+  
+  useEffect(() => {
+    if (selectedItem && modalRef.current) {
+      modalRef.current.showModal();
+    }
+  }, [selectedItem]);
+
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl md:text-3xl lg:text-5xl font-bold">
+        Shop
+      </h1>
+
+      <div className="overflow-x-auto ">
         <table className="table table-zebra">
           {/* head */}
           <thead className="bg-purple-700 text-white">
@@ -18,34 +36,90 @@ const Shop = () => {
               <th>#</th>
               <th>Name</th>
               <th>Category</th>
-              <th>Company Name</th>
-              <th>Generic</th>
               <th>Price</th>
-              <th>See more</th>
+              <th>See Details</th>
               <th>Select</th>
             </tr>
           </thead>
           <tbody>
             {category.map((shop, index) => (
               <tr className="font-bold" key={shop.id}>
-                <th>{index + 1}</th>   
+                <th>{index + 1}</th>
                 <td>{shop.name}</td>
                 <th>{shop.category}</th>
-                <td>{shop.company_name}</td>
-                <td>{shop.generic_name}</td>
                 <td className="">${shop.price}</td>
-                <Link to={`/detailsMedicine/${shop.id}`}>
-                <th><IoEye className="w-8 h-6 text-purple-500"/></th>
-                </Link>
-               
-                <th><button className="btn bg-purple-900 text-white">Select</button></th>
+                <td>
+                <IoEye
+                    onClick={() => openModal(shop)
+                    }
+                    className="text-purple-500 h-8 w-4"
+                  />
+                </td>
+
+                <th>
+                  <button className="btn bg-purple-900 text-white">
+                    Select
+                  </button>
+                </th>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+        {/* Modal */}
+        {selectedItem && (
+      <dialog ref={modalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <div className="flex justify-between">
+            <div>
+                <img src={selectedItem.image} alt="image" />
+            </div>
+            <div className="">
+            <h3 className="font-bold text-lg">Name: {selectedItem.name}</h3>
+            <p className="py-4"><span className="font-bold">Price: </span>
+            {selectedItem.price} $
+          </p>
+            <p className="py-4"><span className="font-bold">Category: </span>
+            {selectedItem.category}
+          </p>
+          <p className="py-4"><span className="font-bold">Company: </span>
+            {selectedItem.company_name}
+          </p>
+          <p className="py-4"><span className="font-bold">Description: </span>
+            {selectedItem.description}
+          </p>
+          <p className="py-4"><span className="font-bold">Generic Name: </span>
+            {selectedItem.generic_name}
+          </p>
+          <p className="py-4"><span className="font-bold">Price Per Unit: </span>
+            {selectedItem.price_per_unit} $
+          </p>
+          <p className="py-4"><span className="font-bold">Item Mass Unit: </span>
+            {selectedItem.item_mass_unit}
+          </p>
+          <p className="py-4"><span className="font-bold">Quantity: </span>
+            {selectedItem.number_of_medicines_in_category} piece
+          </p>
+          <p className="py-4"><span className="font-bold">Discount: </span>
+            {selectedItem.discount_medicine}
+          </p>
+          <p className="py-4"><span className="font-bold">Discount Percentage: </span>
+            {selectedItem.discount_percentage} %
+          </p>
+            </div>
+          </div>
+         
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-    );
+      </dialog>
+        )}
+    </div>
+  );
 };
 
 export default Shop;
+
--- src/pages/Home/Home/CategoryCard/FilterCategory.jsx
+++ src/pages/Home/Home/CategoryCard/FilterCategory.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+import { FaEye } from "react-icons/fa";
 
 const FilterCategory = ({ items, title }) => {
   // console.log(items, "items");
+
+  const [selectedItem, setSelectedItem] = useState(null);
+  // console.log(selectedItem);
+  const modalRef = useRef(null);
+
+  
+  const openModal = (item) => {
+    setSelectedItem(item);
+    document.getElementById('my_modal_5').showModal();
+  };
+  
+  useEffect(() => {
+    if (selectedItem && modalRef.current) {
+      modalRef.current.showModal();
+    }
+  }, [selectedItem]);
+
 
   if (items.length === 0) {
     return null;
@@ -11,38 +30,93 @@ const FilterCategory = ({ items, title }) => {
       <h2 className="text-center text-xl font-bold mt-4">{title}</h2>
 
       <div className="overflow-x-auto">
-
         <table className="table">
           {/* head */}
-          
-         
+
           <thead className="bg-purple-800 text-white">
             <tr>
               <th></th>
               <th>Name</th>
               <th>Category</th>
-              <th></th>
+              <th>See Details</th>
+              <th>Add to Cart</th>
             </tr>
           </thead>
-          
-          <tbody>
-           
-          { items.map((item, index) => (
 
+          <tbody>
+            {items.map((item, index) => (
               <tr key={item.id}>
                 <th>{index + 1}</th>
                 <td>{item.name}</td>
                 <td>{item.category}</td>
-                <td></td>
+                <td>
+                  <FaEye
+                    onClick={() => openModal(item)
+                    }
+                    className="text-purple-500 h-8 w-4"
+                  />
+                </td>
+                <td>
+                  <button className="btn bg-purple-900 text-white">
+                    Select
+                  </button>
+                </td>
               </tr>
-             ))}
-            
-          </tbody> 
-
+            ))}
+          </tbody>
         </table>
-       
       </div>
-
+      {/* Modal */}
+      {selectedItem && (
+      <dialog ref={modalRef} id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <div className="flex justify-between">
+            <div>
+                <img src={selectedItem.image} alt="image" />
+            </div>
+            <div className="">
+            <h3 className="font-bold text-lg">Name: {selectedItem.name}</h3>
+            <p className="py-4"><span className="font-bold">Price: </span>
+            {selectedItem.price} $
+          </p>
+            <p className="py-4"><span className="font-bold">Category: </span>
+            {selectedItem.category}
+          </p>
+          <p className="py-4"><span className="font-bold">Company: </span>
+            {selectedItem.company_name}
+          </p>
+          <p className="py-4"><span className="font-bold">Description: </span>
+            {selectedItem.description}
+          </p>
+          <p className="py-4"><span className="font-bold">Generic Name: </span>
+            {selectedItem.generic_name}
+          </p>
+          <p className="py-4"><span className="font-bold">Price Per Unit: </span>
+            {selectedItem.price_per_unit} $
+          </p>
+          <p className="py-4"><span className="font-bold">Item Mass Unit: </span>
+            {selectedItem.item_mass_unit}
+          </p>
+          <p className="py-4"><span className="font-bold">Quantity: </span>
+            {selectedItem.number_of_medicines_in_category} piece
+          </p>
+          <p className="py-4"><span className="font-bold">Discount: </span>
+            {selectedItem.discount_medicine}
+          </p>
+          <p className="py-4"><span className="font-bold">Discount Percentage: </span>
+            {selectedItem.discount_percentage} %
+          </p>
+            </div>
+          </div>
+         
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+        )}
     </div>
   );
 };
